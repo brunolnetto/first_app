7dcc4118d1bf79dbf8452f792702b66fc29faa4d
Bruno Peixoto <brunolnetto@gmail.com>
2021-11-16 15:27:23 -0300

    Fill the form

--- server.js
+++ server.js
@@ -1,19 +1,49 @@
-// Importing Libs
-const express = require('express');
-const path = require('path');
+/**
+ * Required External Modules
+ */
 
-// Init express
+const express = require("express");
+const path = require("path");
+
+
+/**
+ * App Variables
+ */
+
 const app = express();
-
-// Initial constants
 const PORT = process.env.PORT || 8080;
 
-app.get('/*', (req, res) => {
-  const page = path.join(__dirname, '../public/index.pug');
-  res.sendFile(page);
+// [START enable_parser]
+// This middleware is available in Express v4.16.0 onwards
+app.use(express.json({extended: true}));
+// [END enable_parser]
+
+app.get('/', (req, res) => {
+  res.send('Hello from App Engine!');
 });
 
-// Listening for clients
+// [START add_display_form]
+app.get('/submit', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/form.html'));
+});
+// [END add_display_form]
+
+// [START add_post_handler]
+app.post('/submit', (req, res) => {
+  console.log({
+    name: req.body.name,
+    message: req.body.message,
+  });
+  res.send('Thanks for your message!');
+});
+// [END add_post_handler]
+
+// Listen to the App Engine-specified port, or 8080 otherwise
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
+
+// [END app]
+module.exports = app;
+
+
