9296c215fff60732747c16bd316e6b330c912571
Bruno Peixoto <brunolnetto@gmail.com>
2021-11-20 19:37:35 -0300

    fix: database name

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 # https://stackoverflow.com/questions/18223665/postgresql-query-from-bash-script-as-database-user-postgres
 # Runs su as 'postgres' user & Access psql terminal
 sudo -u postgres -H -- psql -U postgres -c "CREATE USER postgres WITH PASSWORD 'postgres' CREATEDB;"
-sudo -u postgres -H -- psql -U postgres -c "CREATE DATABASE dbtest OWNER postgres;"
+sudo -u postgres -H -- psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE postgres TO postgres;"
+sudo -u postgres -H -- psql -U postgres -c "ALTER USER postgres WITH SUPERUSER;"
 sudo -u postgres -H -- psql -U postgres -c "CREATE TABLE user_messages (name TEXT, message TEXT);"
 sudo -u postgres -H -- psql -U postgres -c "CREATE TABLE users (id UUID PRIMARY KEY, name TEXT NOT NULL);"
 
